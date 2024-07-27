3fd91f2faa5b3a50f1b7c9a8ee9d96a399bcd0d4
AaryanAn <aaryan.r.anand@gmail.com>
2024-07-27 19:00:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,34 @@
+def main():
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode\n2. Decode\n3. Quit')
+        userOption = input('\nPlease enter an option: ')
+        if userOption == 1:
+            encodePass = input("Please enter your password to encode: ")
+            encode()
+        
+        elif userOption == 2:
+            print(f"The encoded password is {encode(encodePass)}, and the original password is {encodePass}.")
+            decode()
 
-
+        elif userOption == 3:
+            break
+        
 def decode(password):
     ans = ""
     for val in password:
         ans += str(int(val) - 3)
     return ans
+
+def encode(password):
+    ans = ""
+    for val in range(len(password)):
+        ans += str(int(val) + 3)
+    return ans
+
+if __name__ == '__main__':
+    main()
+
+
+# Jerry Payne
